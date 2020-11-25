6172c7e8a1e751ad627b8cadc1ec0cecd6a3c3a1
Thestickman391 <stickman391@gmail.com>
2020-11-25 19:14:25 +1300

    Got email working

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PWORD
+  }
+});
+
+var mailOptions = {
+  from: process.env.EMAIL,
+  to: 'stickman391@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
