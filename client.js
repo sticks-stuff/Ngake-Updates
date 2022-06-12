202243f44a4f13857506157c2438e7a13918b55b
charlotte <charlottemackenzi@gmail.com>
2022-06-12 17:55:37 +1200

    completely rewrote the entire thing basically using flickity now also works properly on mobile now (probably)

--- client.js
+++ client.js
@@ -38,111 +38,58 @@ function doData(data) {
 	// Get all entries from spreadsheet
 	var entries = data.valueRanges[0].values;
 
-	// // Set initial previous row, so we can check if the data in the current cell is from a new row
-	// var previousRow = 0;
-
-	// // Iterate all entries in the spreadsheet
-	// for (var i = 0; i < entries.length; i++) {
-	// 	// check what was the latest row we added to our result array, then load it to local variable
-	// 	var latestRow = results[results.length - 1];
-
-	// 	// get current cell
-	// 	var cell = entries[i];
-
-	// 	// get text from current cell
-	// 	var text = cell.content.$t;
-
-	// 	// get the current row
-	// 	var row = cell.gs$cell.row;
-
-	// 	// Determine if the current cell is in the latestRow or is a new row
-	// 	if (row > previousRow) {
-	// 		// this is a new row, create new array for this row
-	// 		var newRow = [];
-
-	// 		// add the cell text to this new row array  
-	// 		newRow.push(text);
-
-	// 		// store the new row array in the final results array
-	// 		results.push(newRow);
-
-	// 		// Increment the previous row, since we added a new row to the final results array
-	// 		previousRow++;
-	// 	} else {
-	// 		// This cell is in an existing row we already added to the results array, add text to this existing row
-	// 		latestRow.push(text);
-	// 	}
-
-	// }
-
-	// entries.shift()
-
 	handleResults(entries);
 }
+
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+  // options
+  lazyLoad: true,
+  pageDots: false
+});
+
+flkty.resize();
+
+window.addEventListener('resize', function(event){
+    flkty.resize();
+	document.getElementsByClassName('flickity-viewport')[0].style.height = window.innerHeight;
+});
+
+flkty.on( 'settle', function( index ) {
+	window.location.hash = encodeURIComponent(index);
+});
 
 // Do what ever you please with the final array
 function handleResults(spreadsheetArray) {
 	console.log(spreadsheetArray);
-	spreadsheetArraySave = spreadsheetArray;
 	row = numberOfScroll || spreadsheetArraySave.length - 1;
 	console.log(numberOfScroll);
-	if (row <= 1) {
-		document.getElementById("leftArrow").style.opacity = 0;
-		document.getElementById("leftArrow").style.visibility = "hidden";
-	}
-	if (row >= (spreadsheetArraySave.length - 1)) {
-		document.getElementById("rightArrow").style.opacity = 0;
-		document.getElementById("rightArrow").style.visibility = "hidden";
-	}
-	loadImage(row);
-}
+	for (let i = spreadsheetArray.length - 1; i > 0; i--) {
+		var element = spreadsheetArray[i];
+		var carouselCell = document.createElement("div");
+		carouselCell.classList.add('carousel-cell');
 
-function loadImage(row) {
-	document.getElementById("image").src = spreadsheetArraySave[row][2];
-	document.getElementById("imageBackground").style.backgroundImage = "url(" + spreadsheetArraySave[row][2] + ")";
-	document.getElementById("timestamp").innerHTML = spreadsheetArraySave[row][0];
-	var caption = document.getElementById("caption");
-	caption.innerHTML = spreadsheetArraySave[row][1];
-	caption.style.top = "calc(" + (Math.floor(Math.random() * (1 + 80 - 20)) + 20) + "% - " + (caption.clientHeight / 2) + "px)";
-	caption.style.left = "calc(" + (Math.floor(Math.random() * (1 + 80 - 20)) + 20) + "% - " + (caption.clientWidth / 2) + "px)";
-}
+		var timestamp = document.createElement("div");
+		timestamp.classList.add('timestamp');
+		timestamp.innerHTML = element[0];
+		carouselCell.appendChild(timestamp);
 
-function rightArrow() {
-	var numberOfScroll = parseInt(decodeURIComponent(window.location.hash.slice(1)));
-	if(numberOfScroll == spreadsheetArraySave.length - 1) {
-		return;
+		var caption = document.createElement("div");
+		caption.classList.add('caption');
+		caption.innerHTML = element[1];
+		carouselCell.appendChild(caption);
+		
+		var image = document.createElement("img");
+		image.classList.add('carousel-cell-image');
+		image.dataset.flickityLazyload = element[2];
+		carouselCell.appendChild(image);
+		
+		flkty.prepend(carouselCell);
+		caption.style.top = "clamp(0px, calc(" + (Math.floor(Math.random() * (1 + 80 - 20)) + 20) + "% - " + (caption.clientHeight / 2) + "px), " + caption.clientHeight + "px)";
+		caption.style.left = "calc(max(calc(" + (Math.floor(Math.random() * (1 + 80 - 20)) + 20) + "% - " + (caption.clientWidth / 2) + "px), 0px)"; //have to do this here bc otherwise the div isnt visible and clientHeight and clientWidth returns 0
 	}
-	row = numberOfScroll || spreadsheetArraySave.length - 1;
-	row++;
-	window.location.hash = encodeURIComponent(row);
-	loadImage(row);
-	if (row > 1) {
-		document.getElementById("leftArrow").style.opacity = 100;
-		document.getElementById("leftArrow").style.visibility = "visible";
-	}
-	if (row >= (spreadsheetArraySave.length - 1)) {
-		document.getElementById("rightArrow").style.opacity = 0;
-		document.getElementById("rightArrow").style.visibility = "hidden";
-	}
-}
-
-function leftArrow() {
-	var numberOfScroll = parseInt(decodeURIComponent(window.location.hash.slice(1)));
-	if(numberOfScroll == 1) {
-		return;
-	}
-	row = numberOfScroll || spreadsheetArraySave.length - 1;
-	row--;
-	window.location.hash = encodeURIComponent(row);
-	loadImage(row);
-	if (row <= 1) {
-		document.getElementById("leftArrow").style.opacity = 0;
-		document.getElementById("leftArrow").style.visibility = "hidden";
-	}
-	if (row < (spreadsheetArraySave.length - 1)) {
-		document.getElementById("rightArrow").style.opacity = 100;
-		document.getElementById("rightArrow").style.visibility = "visible";
-	}
+	// loadImage(row);
+	flkty.select( parseInt(decodeURIComponent(window.location.hash.slice(1))), false, true )
 }
 
 // Get the modal
